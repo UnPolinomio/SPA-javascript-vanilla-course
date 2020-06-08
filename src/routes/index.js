c4467a781a1f043cb75eeb232f5c301a4966fd55
Hector Franco T <tor6.franco6@hotmail.com>
2020-06-08 13:57:44 -0500

    Routes now works

--- src/routes/index.js
+++ src/routes/index.js
@@ -14,7 +14,14 @@ const routes = {
 async function router() {
     const header = null || document.getElementById('Header')
     const content = null || document.getElementById('Content')
+
     header.innerHTML = await Header()
+    let hash = getHash()
+    let route = await resolveRoutes(hash)
+
+    let render = routes[route] ? routes[route] : Error404
+    
+    content.innerHTML = await render()
 }
 
 export default router
